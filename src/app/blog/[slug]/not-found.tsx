b2e5f6b06becdff30bd86df1a57ff3ca7e860c40
Tom Sherman <the.tomsherman@gmail.com>
2023-05-20 14:26:36 +0100

    Add not found

---
+++ src/app/blog/[slug]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="container">
+      <h1>404 Not Found</h1>
+      <p>Sorry, that post seems to be missng 🤔</p>
+    </div>
+  );
+}
