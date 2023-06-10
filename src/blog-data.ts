7799ad0693a6710bd8e4b6a71b1f6a3d30434ffa
Tom Sherman <the.tomsherman@gmail.com>
2023-06-10 12:22:48 +0100

    Migrate blog page away from redis

--- src/blog-data.ts
+++ src/blog-data.ts
@@ -1,20 +1,82 @@
-import "server-only";
+import { request as githubRequest } from "@octokit/request";
+import { GITHUB_TOKEN } from "./server-constants";
 import { z } from "zod";
-import type { request as githubRequest } from "@octokit/request";
-import { type VercelKV, kv } from "@vercel/kv";
-import { cache } from "react";
+
+const github = githubRequest.defaults({
+  headers: {
+    authorization: `token ${GITHUB_TOKEN}`,
+    accept: "application/vnd.github.v3+json",
+  },
+});
+
+export async function getPostByPath(path: string) {
+  const slug = getSlugFromPath(path); // fail fast if path is invalid
+  const res = await github(`GET /repos/{owner}/{repo}/contents/{path}`, {
+    owner: "tom-sherman",
+    repo: "blog",
+    path,
+    headers: {
+      accept: "application/vnd.github.v3.raw",
+    },
+  });
+
+  if (typeof res.data !== "string") {
+    throw new Error("Failed to get contents");
+  }
+
+  const content = res.data as string;
+  const fontMatter = parseFrontMatter(content);
+
+  return {
+    content: content.slice(fontMatter.contentStart),
+    slug,
+    ...fontMatter.attributes,
+  };
+}
+
+type ReadonlyMap<K, V> = Omit<Map<K, V>, "set" | "delete">;
+
+export async function getPathSlugMappings() {
+  const res = await github(`GET /repos/{owner}/{repo}/contents/{path}`, {
+    owner: "tom-sherman",
+    repo: "blog",
+    path: "posts",
+  });
+
+  if (!Array.isArray(res.data)) {
+    throw new Error("Failed to get contents");
+  }
+
+  const pathToSlugEntries = res.data.map(
+    (item) => [item.path, getSlugFromPath(item.path)] as const
+  );
+
+  return {
+    pathToSlug: new Map(pathToSlugEntries) as ReadonlyMap<string, string>,
+    slugToPath: new Map(
+      pathToSlugEntries.map(([path, slug]) => [slug, path])
+    ) as ReadonlyMap<string, string>,
+  };
+}
+
+function getSlugFromPath(path: string) {
+  const match = /^posts\/\d+-(.+)\.md$/gi.exec(path)?.[1];
+  if (!match) {
+    throw new Error(`Failed to get slug from path: ${path}`);
+  }
+
+  return match;
+}
 
 const frontMatterTagsSchema = z.array(z.string());
 const frontMatterStatusSchema = z.union([
   z.literal("unlisted"),
   z.literal("published"),
 ]);
-
 const frontMatterSchema = z.object({
   title: z.string(),
   createdAt: z.string(),
   tags: frontMatterTagsSchema.default([]),
-  slug: z.string(),
   status: frontMatterStatusSchema.default("published"),
   description: z
     .string()
@@ -22,200 +84,6 @@ const frontMatterSchema = z.object({
     .optional()
     .transform((value) => value ?? null),
 });
-
-interface PostMeta extends z.TypeOf<typeof frontMatterSchema> {
-  path: string;
-}
-
-interface BlogPost extends PostMeta {
-  content: string;
-  lastModifiedAt: string | null;
-}
-
-export interface BlogData {
-  getPost: (slug: string) => Promise<BlogPost | null>;
-  listAllPosts: () => Promise<PostMeta[]>;
-}
-
-type GitHubClient = ReturnType<
-  typeof githubRequest.defaults<{
-    headers: {
-      authorization: string;
-      accept: string;
-    };
-  }>
->;
-
-export class GitHubBlogData implements BlogData {
-  #gh: GitHubClient;
-
-  constructor(gh: GitHubClient) {
-    this.#gh = gh;
-  }
-
-  async getPost(slug: string) {
-    return (
-      (await this.#listAllPostsWithContents()).find(
-        (post) => post.slug === slug
-      ) ?? null
-    );
-  }
-
-  async listAllPosts() {
-    return (await this.#listAllPostsWithContents())
-      .map(({ content, ...meta }) => meta)
-      .sort(
-        (a, b) =>
-          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-      );
-  }
-
-  async #getPostDataByPath(path: string) {
-    const content = await this.#getRawFileContents(path);
-    const fontMatter = parseFrontMatter(content);
-
-    return {
-      content: content.slice(fontMatter.contentStart),
-      ...fontMatter.attributes,
-    };
-  }
-
-  async getPostSlugByPath(path: string) {
-    const postData = await this.#getPostDataByPath(path);
-    return postData.slug;
-  }
-
-  async getPostByPath(path: string) {
-    const postData = await this.#getPostDataByPath(path);
-
-    const commits = await this.#gh(`GET /repos/{owner}/{repo}/commits`, {
-      owner: "tom-sherman",
-      repo: "blog",
-      path,
-    });
-
-    return {
-      path,
-      ...postData,
-      lastModifiedAt:
-        commits.data.length < 2
-          ? null
-          : commits.data[0]?.commit.committer?.date ?? null,
-    };
-  }
-
-  async #listAllPostsWithContents() {
-    const result = await this.#gh(`GET /repos/{owner}/{repo}/contents/posts/`, {
-      owner: "tom-sherman",
-      repo: "blog",
-    });
-
-    const files = z
-      .array(
-        z.object({
-          type: z.literal("file"),
-          path: z.string(),
-          sha: z.string(),
-        })
-      )
-      .parse(result.data);
-
-    return Promise.all(files.map(({ path }) => this.getPostByPath(path)));
-  }
-
-  async #getRawFileContents(path: string) {
-    const contents = await this.#gh(
-      `GET /repos/{owner}/{repo}/contents/{path}`,
-      {
-        owner: "tom-sherman",
-        repo: "blog",
-        path,
-        headers: {
-          accept: "application/vnd.github.v3.raw",
-        },
-      }
-    );
-
-    return parseContentsResponse(contents);
-  }
-}
-
-export class VercelKvBlogData implements BlogData {
-  #kv: VercelKV;
-
-  constructor(kv: VercelKV) {
-    this.#kv = kv;
-  }
-
-  async getPost(slug: string): Promise<BlogPost | null> {
-    const post = await this.#kv.get(`posts/${slug}`);
-
-    if (!post || frontMatterSchema.safeParse(post).success === false) {
-      return null;
-    }
-
-    return post as BlogPost;
-  }
-
-  async listAllPosts(): Promise<PostMeta[]> {
-    const keys = await this.#kv.keys("*");
-
-    const posts = await Promise.all(
-      keys.map(async (key) => {
-        const post = await this.#kv.get(key);
-
-        if (post == null) {
-          throw new Error("Failed to get post");
-        }
-
-        return {
-          ...frontMatterSchema.parse(post),
-          path: key,
-        };
-      })
-    );
-
-    return posts
-      .filter((post) => post.status === "published")
-      .sort(
-        (a, b) =>
-          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-      );
-  }
-
-  async deletePostsByPath(paths: string[]) {
-    if (paths.length === 0) {
-      return;
-    }
-    await this.#kv.del(...paths);
-  }
-
-  async upsertPosts(posts: BlogPost[]) {
-    const tx = this.#kv.multi();
-
-    for (const post of posts) {
-      tx.set(post.path, JSON.stringify(post));
-    }
-
-    await tx.exec();
-  }
-}
-
-export const blog = cache(() => new VercelKvBlogData(kv));
-export const listAllPosts = cache(() => blog().listAllPosts());
-
-type ContentsApiResponse = Awaited<
-  ReturnType<typeof githubRequest<"GET /repos/{owner}/{repo}/contents/{path}">>
->;
-
-function parseContentsResponse(res: ContentsApiResponse) {
-  if (typeof res.data === "string") {
-    return res.data as string;
-  }
-
-  throw new Error("Failed to get contents");
-}
-
 const frontMatterRe = /^---$(?<frontMatter>.*?)^---$/ms;
 const frontMatterKeyValueRe = /^(?<key>[^:]+):(?<value>.*)$/m;
 function parseFrontMatter(input: string) {
