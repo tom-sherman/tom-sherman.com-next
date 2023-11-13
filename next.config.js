72dc76f8c0260097dc8309f051f2b41f94d3f54c
Tom Sherman <the.tomsherman@gmail.com>
2023-11-13 22:57:28 +0000

    Add resume path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => [
+    {
+      source: "/resume",
+      destination: "/CV.pdf",
+    },
+  ],
 };
 
 module.exports = nextConfig;
