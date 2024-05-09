a7b37fec988f48ccb9fa6f5f3f8c85b4f9c5d046
Tom Sherman <the.tomsherman@gmail.com>
2024-05-09 15:24:27 +0100

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" />
+      </head>
       <body className={styles.body}>
         {children}
         <footer className="container">
