5359c1b04fe928fef5c1cf106d19609ae0570e28
Tom Sherman <the.tomsherman@gmail.com>
2025-04-22 22:50:22 +0100

    Update prettier

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const MASTODON_URL = "https://fosstodon.org/@tomsherman";
-export const BLUESKY_URL = "https://bsky.app/profile/did:plc:2xau7wbgdq4phuou2ypwuen7";
+export const BLUESKY_URL =
+  "https://bsky.app/profile/did:plc:2xau7wbgdq4phuou2ypwuen7";
 export const GITHUB_URL = "https://github.com/tom-sherman";
 export const LINKEDIN_URL =
   "https://www.linkedin.com/in/tom-sherman-2a2aa0136/";
